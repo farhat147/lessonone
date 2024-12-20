39bdf33dd74e0b5bdcac42ae0018a37eaa63f52a
unknown <farhat147>
2024-12-20 21:23:06 +0300

    factorial

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+using namespace std;
+int main()
+{
+  int result =0;
+  int key =0;
+
+  cin>> key;
+
+  for (int i = 1; i <=key; i++) {
+      cout << i;
+      result *= 1;
+  }
+
+
+
+
+return 0;
+
+}
